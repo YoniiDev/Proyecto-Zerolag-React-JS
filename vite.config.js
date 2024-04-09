1e9b627cae280441aa84204e7c456fa85329e3dd
Yonathan Bravo <Yonathan.ybf@gmail.com>
2024-04-09 16:12:51 -0400

    actualizacion del vite config2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import vitePluginSass  from 'vite-plugin-sass'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginSass()],
-  css:{preprocessorOptions:{sass:{}}}
+  plugins: [react()]
 })
